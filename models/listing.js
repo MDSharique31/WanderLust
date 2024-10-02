a70c30e89fe431da20dcfabbdf898ace7f361aa4
MDSharique31 <shariquevivo@gmail.com>
2024-10-02 20:31:49 +0530

    Added listingSchema

--- models/listing.js
+++ models/listing.js
@@ -1,6 +1,8 @@
+// Import mongoose for MongoDB interactions
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const Review = require("./review.js");
+
 
 const listingSchema = new Schema({
   title: {
