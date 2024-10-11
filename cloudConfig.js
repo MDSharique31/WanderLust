00e2978c83ffcfd46615b0725029ec9b0ac2af89
MDSharique31 <shariquevivo@gmail.com>
2024-10-11 16:51:40 +0530

    Configured Cloudinary storage

--- cloudConfig.js
+++ cloudConfig.js
@@ -2,6 +2,7 @@
 const cloudinary = require("cloudinary").v2;
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 
+// Configure Cloudinary with environment variables for secure access
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.CLOUD_API_KEY,
